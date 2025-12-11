c4457524731cdc6c7b5cef063060f78a24377a33
ainacteru <gbenawijr@gmail.com>
2025-12-10 21:53:25 -0600

    figured out how to manipulate strings i'm so cool

--- src/main.py
+++ src/main.py
@@ -1,5 +1,10 @@
+from pathlib import Path
 from FileManagement.VisualSelector import VisualSelector
+from FileManagement.LocationManager import LocationManager
 
-vs = VisualSelector()
-vs.select_file()
-vs.select_directory()
+# vs = VisualSelector()
+# vs.select_file()
+# vs.select_directory()
+
+lm = LocationManager()
+lm.getDeviceLocation()
